c8806b3d5f9a9f43c10fe34eab53c07580096b15
andrleon <andreyko.l.a@gmail.com>
2018-08-02 09:37:10 +0300

    Class LinkedObjects is renamed

--- src/com/luxoft/oop/mytasks/task_2a/Demo_Task2.java
+++ src/com/luxoft/oop/mytasks/task_2a/Demo_Task2.java
@@ -4,11 +4,11 @@ public class Demo_Task2 {
 
     public static void main(String[] args) {
 
-        linkedObjects[] lo = new linkedObjects[4];
-        lo[0] = new linkedObjects("O1");
-        lo[1] = new linkedObjects("O2");
-        lo[2] = new linkedObjects("O3");
-        lo[3] = new linkedObjects("O4");
+        LinkedObjects[] lo = new LinkedObjects[4];
+        lo[0] = new LinkedObjects("O1");
+        lo[1] = new LinkedObjects("O2");
+        lo[2] = new LinkedObjects("O3");
+        lo[3] = new LinkedObjects("O4");
 
 
         lo[0].linkWithObject(lo[1]);
@@ -18,7 +18,7 @@ public class Demo_Task2 {
      //   lo[0].showNext();
 
 
-        for(linkedObjects obj : lo){
+        for(LinkedObjects obj : lo){
             obj.showNext();
 
       }
